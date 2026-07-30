7a474d171e271aa53703e160fbd165f810c0e336
Brian Mwangi <briankinyua0101@gmail.com>
2026-07-30 23:51:43 +0300

    add routes for SmsSimulator and OperationalReadiness components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { MainLayout } from "./components/layout/MainLayout";
 import { Dashboard } from "./pages/Dashboard";
 import { Observatory } from "./pages/Observatory";
 import { Playbooks } from "./pages/Playbooks";
+import { SmsSimulator } from "./pages/SmsSimulator";
+import { OperationalReadiness } from "./pages/OperationalReadiness";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="playbooks" element={<Playbooks />} />
         <Route path="observatory" element={<Observatory />} />
+        <Route path="sms-simulator" element={<SmsSimulator />} />
+        <Route path="readiness" element={<OperationalReadiness />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
